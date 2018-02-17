c3da8f3b6ffcffe05cc23b162b894d6bd4799f42
carlosrpcaldas <Moises12>
2018-02-17 11:24:01 -0200

    Segunda parte

--- app/src/main/java/com/example/logonrm/demoorderedbroadcast/BootBroadcastReceiver.kt
+++ app/src/main/java/com/example/logonrm/demoorderedbroadcast/BootBroadcastReceiver.kt
@@ -3,14 +3,15 @@ package com.example.logonrm.demoorderedbroadcast
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
+import android.widget.Toast
 
 /**
  * Created by logonrm on 17/02/2018.
  */
-class MeuOutroReceiver: BroadcastReceiver() {
+class BootBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        Log.i("DEMOORDERED", "Meu outro receiver")
+
+        Toast.makeText(context, "Terminei a inicializacao", Toast.LENGTH_SHORT).show()
 
     }
 }
--- app/src/main/java/com/example/logonrm/demoorderedbroadcast/MainActivity.kt
+++ app/src/main/java/com/example/logonrm/demoorderedbroadcast/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.logonrm.demoorderedbroadcast
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var receiver: BroadcastReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +18,25 @@ class MainActivity : AppCompatActivity() {
         btEnviar.setOnClickListener{
             sendBroadcast(Intent("MeuOrderedBroadcast"))
         }
+
+        var filter = IntentFilter()
+        filter.addAction(Intent.ACTION_POWER_CONNECTED)
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED)
+
+        receiver = object : BroadcastReceiver(){
+            override fun onReceive(context: Context?, intent: Intent?) {
+                if(intent?.action.equals(Intent.ACTION_POWER_CONNECTED)){
+                    tvUSBStatus.text = "Cabo conectado"
+                }else{
+                tvUSBStatus.text = "Cabo desconectado"
+            }
+        }
+
+    }
+        registerReceiver(receiver, filter)
+}
+    override fun onDestroy(){
+    unregisterReceiver(receiver)
+    super.onDestroy()
     }
 }
