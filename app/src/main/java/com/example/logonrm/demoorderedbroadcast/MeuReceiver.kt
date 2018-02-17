ec3fa67c00c55572e7cf6879ccd4dee1f24bcef3
logonrm <logonrm@fiap.com.br>
2018-02-17 09:34:54 -0200

    Primeira parte

---
+++ app/src/main/java/com/example/logonrm/demoorderedbroadcast/MeuReceiver.kt
@@ -0,0 +1,14 @@
+package com.example.logonrm.demoorderedbroadcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+/**
+ * Created by logonrm on 17/02/2018.
+ */
+class MeuReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
